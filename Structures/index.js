60754f2c6800244d9463cc2a0f2e96e5faf33b5d
BullDoze <xthunderx.krew@gmail.com>
2022-10-29 08:31:08 +0530

    trial

--- Structures/index.js
+++ Structures/index.js
@@ -29,3 +29,19 @@ module.exports = client
 
 client.login('MTAzNTE1Mjg5MTEyOTg4ODg5OQ.GQ54Yj.NPVZSZufzgSQT8bekYLY2ByqLx2FlVA0p6d070')
 // keepAlive()
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+  console.log(" [Error_Handling] :: Multiple Resolves");
+  console.log(type, promise, reason);
+});
